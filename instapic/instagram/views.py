0c06af1d439e83fc7284a9a3811b9ba3e178d5eb
patrickNgare <patrickngare@gmail.com>
2018-03-03 10:32:49 +0300

    addedd method to querry images

--- instapic/instagram/views.py
+++ instapic/instagram/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Image,Profile,Comment
 
 
 def index(request):
     
+    updates=Image.get_image_by_id()
+    
 
-    return render(request,'all-temps/index.html')
+    return render(request,'all-temps/index.html'{"updates":updates})
