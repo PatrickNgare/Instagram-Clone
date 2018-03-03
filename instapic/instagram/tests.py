7ec1a12ac4cf80c743ded01d0ff87d1033cfb500
patrickNgare <patrickngare@gmail.com>
2018-03-03 21:47:42 +0300

    Created image test  class

--- instapic/instagram/tests.py
+++ instapic/instagram/tests.py
@@ -3,3 +3,23 @@ import datetime as dt
 from .models import Image, Profile
 from django.contrib.auth.models import User
 
+
+class ImageTestClass(TestCase):
+     
+    def setUp(self):
+         self.image=Image(image='imagelink',image_name="coding",image_caption="amazing laptop",likes="456",profile=patel)
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+
+    def test_delete(self):
+        self.image.save_image()
+        images=Image.objects.all()
+        self.image.delete_image()
+        images=Image.objects.all()
+        self.assertTrue(len(images)==0)
+
+
+
