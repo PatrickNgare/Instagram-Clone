0c142834a84cafba18c651418d119d580c0b9bdd
patrickNgare <patrickngare@gmail.com>
2018-03-04 07:44:39 +0300

    Added login authentication

--- instapic/instagram/views.py
+++ instapic/instagram/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from .models import Image,Profile,Comment
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     
-    # update=Image.objects.order_by('-postdate')
-    update=Image.objects.all()
-    print(update)
+    update= Image.objects.order_by('-postdate')
+    profile= Profile.objects.order_by('-update_time')
+    comments=Comment.objects.order_by('-timecomment')
+    
     
 
-    return render(request,'all-temps/index.html',{"update":update})
+    return render(request,'all-temps/index.html',{"update":update,"profile":profile,"comments":comments})
 
-
+@login_required(login_url='/accounts/login/')
 def profile(request):
 
     profile=Profile.get_profile()   
