bba35b38efbc9a2521e46d9efd6c81a618755402
patrickNgare <patrickngare@gmail.com>
2018-03-05 10:04:45 +0300

    Added current user to get the user

--- instapic/instagram/views.py
+++ instapic/instagram/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def index(request):
     
+    current_user=request.user
     update= Image.objects.order_by('-postdate')
     profile= Profile.objects.order_by('-update_time')
     comments=Comment.objects.order_by('-timecomment')
