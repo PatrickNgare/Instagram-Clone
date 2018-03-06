f61fccc37253e3bd2922445830498332d4d78373
patrickNgare <patrickngare@gmail.com>
2018-03-06 16:37:52 +0300

    importing upload file

--- instapic/instagram/views.py
+++ instapic/instagram/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Image,Profile,Comment
 from django.contrib.auth.decorators import login_required
+from .forms import UploadForm
 
 @login_required(login_url='/accounts/login/')
 def index(request):
@@ -20,3 +21,45 @@ def profile(request):
     profile=Profile.get_profile()   
 
     return render(request,'all-temps/profile.html',{"profile":profile}) 
+
+
+@login_required(login_url='/accounts/login')
+def user_profile(request, user_id):
+    profile = Profile.objects.get(id=user_id)
+    images = Image.objects.all().filter(user_id=user_id)
+    return render(request, 'all-temps/singleprofile.html', {'profile':profile, 'images':images})    
+
+
+def search_results(request):
+    
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-temps/search.html',{"message":message,"profiles": searched_profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-temps/search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def upload(request):
+    current_user = request.user
+    title = 'Instagrum | Upload'
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        form = UploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.user = current_user
+            upload.profile = profile
+            upload.save()
+            return redirect('index')
+        else:
+            form = UploadForm()
+    return render(request,'all-temps/upload.html',{"title":title, "user":current_user,"form":form})
+
+
+
+
