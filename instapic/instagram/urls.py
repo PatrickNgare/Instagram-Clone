48fcf8f3dda7787d8708de94b884c7d5c71f6879
patrickNgare <patrickngare@gmail.com>
2018-03-06 16:37:24 +0300

    added urls to navigate the app

--- instapic/instagram/urls.py
+++ instapic/instagram/urls.py
@@ -7,6 +7,10 @@ urlpatterns=[
 
     url(r'^$',views.index, name='index'),
     url(r'^profile/',views.profile, name='profile'),
+    url(r'^user/(\d+)', views.user_profile, name='userprofiles'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url('upload/',views.upload, name='upload'),
+
 
 ]
 if settings.DEBUG:
