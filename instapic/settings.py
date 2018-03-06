1250ce5ea49ab5e4ae345e563ef661e4c0329677
patrickNgare <patrickngare@gmail.com>
2018-03-06 17:27:09 +0300

    project refectoring for production

--- instapic/settings.py
+++ instapic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v$49l+qd&h3hkwu5993_0#symmy-d2$x^&lneb+seo2g+bwrwp'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG=config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -85,6 +87,10 @@ DATABASES = {
     }   
 }
 
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -123,9 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS=[ os.path.join(BASE_DIR,"static"),
+  ]
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+STATIFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
